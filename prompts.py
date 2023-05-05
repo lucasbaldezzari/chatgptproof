5278a692898b9bcf8269553e901a607de605baf3
lucasbaldezzari <lmbaldezzari@gmail.com>
2023-05-05 20:54:53 -0300

    Primer commit

---
+++ prompts.py
@@ -0,0 +1,34 @@
+"""Primeros pasos con chatgpt y buenas prácticas de prompts."""
+
+import configparser
+import openai
+import os
+
+config = configparser.ConfigParser()
+config.read("keys.ini")
+
+APIkey = config["openai"]["API_Key"]
+
+openai.api_key  = APIkey
+
+def getCompletion(prompt, modelo="gpt-3.5-turbo"):
+    """Genera una respuesta a partir de un prompt y un modelo de GPT.
+    
+    Por defecto el modelo es gpt-3.5-turbo, que es el que mejor resultados
+    ha dado hasta ahora."""
+
+    messages = [{"role": "user", "content": prompt}]
+
+    response = openai.ChatCompletion.create(
+        model=modelo,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+
+    return response.choices[0].message["content"]
+
+text = """Hola, soy un bot que te ayuda a aprender a programar en Python."""
+promt = f"""Por favor, usar el texto en triple backsticks ```{text}``` para identificar qué quiere el bot"""
+
+response = getCompletion(promt)
+print(response) #nice!
---
+++ readme.md
@@ -0,0 +1,2 @@
+Aprendiendo a usar la libreria _openai_ para hacer aplicaciones del tipo chatbot u otras.
+
